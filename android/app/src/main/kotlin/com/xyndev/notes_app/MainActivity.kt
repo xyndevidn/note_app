a109a00192c75bf419aa449a07a5f5b5df0e33fa
“Xyndev” <xyndevidn@gmail.com>
2023-08-28 12:21:51 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/xyndev/notes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xyndev.notes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'db_provider.dart';
+import 'note_list_page.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DbProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const NoteListPage(),
+      ),
+    );
+  }
+}
